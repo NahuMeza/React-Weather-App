787b5223e6d539d794e59ceba52e98b0408a1c8f
NahuMeza <nahuelgmeza@gmail.com>
2023-05-28 13:21:46 -0300

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import { ReactLogo } from '../Icons/ReactLogo.jsx'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <header>
+      <h1> <ReactLogo /> Weather App</h1>
+    </header>
+  )
+}
