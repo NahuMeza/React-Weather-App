ce1767c69125bae94f1797a75fd2145501b0c41a
NahuMeza <nahuelgmeza@gmail.com>
2023-05-30 21:12:46 -0300

    fixed leaked key

--- src/Hooks/Cords.js
+++ src/Hooks/Cords.js
@@ -1,7 +1,6 @@
 import { useRef } from 'react'
 export function Cords () {
   const searchValue = useRef('')
-  const API_KEY = 'd806c0e719df32b6c26dad2bd10f4879'
 
   const handleSumbit = (e) => {
     e.preventDefault()
@@ -9,7 +8,7 @@ export function Cords () {
   }
 
   const getCords = () => {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchValue.current.value}&limit=1&appid=${API_KEY}`)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchValue.current.value}&limit=1&appid=${import.meta.env.VITE_API_KEY}`)
       .then(response => response.json())
       .then(data => console.log(data))
   }
